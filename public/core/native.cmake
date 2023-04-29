03d84f52f222cf708a42922d225200ad220a851f
Khalyutin <khalyutin@notesoft.net>
2023-04-29 13:32:16 +0300

    -

--- public/core/native.cmake
+++ public/core/native.cmake
@@ -1,11 +1,23 @@
-# Native project initialization setup here.
-# Parse project version etc...
 
-set(enProjectName Project-envelope)
 
-# ...
+macro(enInitNative)
 
-# macro(enInitNative)
-#    ...
-# endmacro()
-# enInitNative()
+    set(enProjectName wxWidjets-envelope)
+
+    file(READ "${CMAKE_SOURCE_DIR}/native/include/wx/version.h" enVERSION_H_CONTENTS)
+    string(REGEX MATCH "wxMAJOR_VERSION[ \t]+([0-9]+)"          enMAJOR_VERSION  ${enVERSION_H_CONTENTS})
+    string(REGEX MATCH "([0-9]+)"                               enMAJOR_VERSION  ${enMAJOR_VERSION})
+    string(REGEX MATCH "wxMINOR_VERSION[ \t]+([0-9]+)"          enMINOR_VERSION  ${enVERSION_H_CONTENTS})
+    string(REGEX MATCH "([0-9]+)"                               enMINOR_VERSION  ${enMINOR_VERSION})
+    string(REGEX MATCH "wxRELEASE_NUMBER[ \t]+([0-9]+)"         enRELEASE_NUMBER ${enVERSION_H_CONTENTS})
+    string(REGEX MATCH "([0-9]+)"                               enRELEASE_NUMBER ${enRELEASE_NUMBER})
+    set(enVERSION ${enMAJOR_VERSION}.${enMINOR_VERSION}.${enRELEASE_NUMBER})
+
+    message(STATUS "=================================================================")
+    message(STATUS "envelope for wxWidgets version : ${enVERSION}")
+    message(STATUS "=================================================================")
+
+endmacro()
+
+
+enInitNative()
