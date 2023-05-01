1a5ed412ca4bae52158778d4e03acc401727e648
Khalyutin <khalyutin@notesoft.net>
2023-05-01 13:38:29 +0300

    -

--- public/items/implemenation/100000_msys2_implib.cmake
+++ public/items/implemenation/100000_msys2_implib.cmake
@@ -1,5 +1,5 @@
 
-if (enIsMSV AND enIsClang)
+if (enIsMSW AND enIsClang)
     # by default msys2 convert option  /implib:../myLib.lib  incorrect as implib;myLib.lib 
     message(STATUS "! enventory variable MSYS2_ARG_CONV_EXCL is: [$ENV{MSYS2_ARG_CONV_EXCL}] mast containig [/implib:]" )
     message(STATUS "! set MSYS2_ARG_CONV_EXCL=/implib" )
