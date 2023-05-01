86c3020f2e6fd52b7c21efadb798ec7de23768f3
Khalyutin <khalyutin@notesoft.net>
2023-05-01 14:57:16 +0300

    evMSYS2_ARG_CONV_EXCL

--- public/items/implemenation/100000_msys2_implib.cmake
+++ public/items/implemenation/100000_msys2_implib.cmake
@@ -1,7 +1,8 @@
 
+
 if (enIsMSW AND enIsClang)
-    # by default msys2 convert option  /implib:../myLib.lib  incorrect as implib;myLib.lib 
-    message(STATUS "! enventory variable MSYS2_ARG_CONV_EXCL is: [$ENV{MSYS2_ARG_CONV_EXCL}] mast containig [/implib:]" )
-    message(STATUS "! set MSYS2_ARG_CONV_EXCL=/implib" )
-    message(STATUS "! see https://www.msys2.org/docs/filesystem-paths/")
+    set(evMSYS2_ARG_CONV_EXCL  $ENV{MSYS2_ARG_CONV_EXCL})
+    if (NOT evMSYS2_ARG_CONV_EXCL STREQUAL "*" AND NOT ${evMSYS2_ARG_CONV_EXCL} MATCHES "./implib.")
+        message(FATAL_ERROR " !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! \n enventory variable MSYS2_ARG_CONV_EXCL is: [$ENV{MSYS2_ARG_CONV_EXCL}] \n mast containig [/implib:] or [*] set MSYS2_ARG_CONV_EXCL=/implib \n see https://www.msys2.org/docs/filesystem-paths/ \n !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!" )
+    endif()
 endif()
