f00f215d23f965fdb2fa37ec8bbf97a493c00e24
Khalyutin <khalyutin@notesoft.net>
2023-05-01 13:02:50 +0300

    add envent.cmake

--- public/items/implemenation/100000_cmake_rc_res_res.cmake
+++ public/items/implemenation/100000_cmake_rc_res_res.cmake
@@ -1,5 +1,5 @@
 
-if (WIN32 AND "${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang")
-    message(STATUS "========== by default CMake with llvm-rc create file with .rc.res.res extention instade of .rc.res") # -c65001    
+if (enIsMSV AND enIsClang)
+    message(STATUS "! bugfix: by default CMake with llvm-rc create file with .rc.res.res extention instade of .rc.res") # -c65001    
     set(CMAKE_RC_COMPILE_OBJECT "<CMAKE_RC_COMPILER> <DEFINES> ${enRC_FLAGS} -fo <OBJECT> <SOURCE>")
 endif()
