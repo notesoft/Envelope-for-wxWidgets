d48c3554d96bac54a6f4faff0f157cd7ba43dadb
khalyutin <khalyutin@notesoft.net>
2023-05-02 10:00:28 +0300

    -

--- public/items/implemenation/100000_msys2_implib.cmake
+++ public/items/implemenation/100000_msys2_implib.cmake
@@ -5,8 +5,8 @@ if (enIsMSW AND enIsClang)
     set(evMSYS2_ARG_CONV_EXCL  $ENV{MSYS2_ARG_CONV_EXCL})
     string(FIND "/implib:" ${evMSYS2_ARG_CONV_EXCL} enPos)
 
-    if (NOT evMSYS2_ARG_CONV_EXCL STREQUAL "*" AND ${enPos} EQUAL -1 )
-        message(FATAL_ERROR " !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! \n enventory variable MSYS2_ARG_CONV_EXCL is: [$ENV{MSYS2_ARG_CONV_EXCL}] \n mast containig [/implib:] or [*] set MSYS2_ARG_CONV_EXCL=/implib \n see https://www.msys2.org/docs/filesystem-paths/ \n !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!" )
+    if (NOT evMSYS2_ARG_CONV_EXCL STREQUAL "*" AND ${enPos} EQUAL (-1) )
+        message(" !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! \n enventory variable MSYS2_ARG_CONV_EXCL is: [$ENV{MSYS2_ARG_CONV_EXCL}] \n mast containig [/implib:] or [*] set MSYS2_ARG_CONV_EXCL=/implib \n see https://www.msys2.org/docs/filesystem-paths/ \n !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!" )
     endif()
     
 endif()
